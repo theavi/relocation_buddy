afdadbd6f7b05e9462386b04849ebd32299fbc77
Avinash <khadsanavinash01@gmail.com>
2025-01-29 21:49:18 +0530

    First commit

---
+++ src/main/java/com/relocation/buddy/RelocationBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.relocation.buddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelocationBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelocationBuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/relocation/buddy/RelocationBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relocation.buddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelocationBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
