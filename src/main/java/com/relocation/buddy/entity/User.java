c7b3212746ad823d90410f6ab022f443a3bcf053
swaraj8020 <srpurarkar@acpce.ac.in>
2025-02-07 22:26:25 +0530

    RLB-05 User Entity Created

--- src/main/java/com/relocation/buddy/entity/User.java
+++ src/main/java/com/relocation/buddy/entity/User.java
@@ -8,4 +8,9 @@ public class User extends  BaseEntity {
     @Id
     private Integer id;
     private String name;
+    private String firstName;
+    private String middleName;
+    private String surname;
+    private String username;
+    private String password;
 }
