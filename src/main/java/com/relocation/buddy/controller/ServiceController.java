f77af758868bae74547051342cd19ded058911d7
sushma <sushvairal01@gmail.com>
2025-03-04 19:49:46 +0530

    create rlbservice completed

--- src/main/java/com/relocation/buddy/controller/ServiceController.java
+++ src/main/java/com/relocation/buddy/controller/ServiceController.java
@@ -5,20 +5,22 @@ import com.relocation.buddy.service.IRLBService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/service")
 public class ServiceController {
-@Autowired
-private IRLBService irlbService;
-@PostMapping("/create")
-    public String create(Model model){
+
+    @Autowired
+    private IRLBService irlbService;
+
+    @GetMapping("/create")
+    public String create(Model model) {
         System.out.println("HTTP get initiated for  create service");
-        model.addAttribute("serviceObject",new ServiceDto());
-        model.addAttribute("path","/service/createRlbService");
-        model.addAttribute("fragment","createRlbService");
+        model.addAttribute("serviceObject", new ServiceDto());
+        model.addAttribute("path", "/rlbservice/createRlbService");
+        model.addAttribute("fragment", "createRlbService");
         return "default";
     }
 }
